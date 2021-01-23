0d84ffa20ad9938ad80b109134d721dc08b2a8e9
Jezer Jojo <jezer.vallivattam@students.iiserpune.ac.in>
2021-01-23 17:26:12 +0530

    ALLOWED_HOSTS set

--- examPortal/settings.py
+++ examPortal/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +25,7 @@ SECRET_KEY = 'r0yc*776ty^e9*02oos(f44rccz%+3@zx5fv-s6uw%b05tn5wo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mimamsa-exam-portal.herokuapp.com']
 
 
 # Application definition
