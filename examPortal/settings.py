3fc21ad5dccaeda46d05b44bae03bd0e4e79d335
Jezer Jojo <jezer.vallivattam@students.iiserpune.ac.in>
2021-02-18 23:59:29 +0530

    Database settings

--- examPortal/settings.py
+++ examPortal/settings.py
@@ -82,6 +82,18 @@ DATABASES = {
     }
 }
 
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
 AUTH_USER_MODEL = "examPortalApp.User"
 
 # Password validation
