599a99e6a5cd3fee46e6eae0f60304c106003553
Jezer Jojo <jezer.vallivattam@students.iiserpune.ac.in>
2021-02-25 18:17:52 +0530

    sqlite database

--- examPortal/settings.py
+++ examPortal/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'examPortal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mimamsa',
-        'USER': 'postgres',
-        'PASSWORD': 'mimamsaAWS14',
-        'HOST': 'database-2.cxa9mi9i4k9z.ap-south-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
