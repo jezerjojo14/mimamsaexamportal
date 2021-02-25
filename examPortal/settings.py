32df1cfd07b1bececcef2ccf622a41a322a8ef82
Jezer Jojo <jezer.vallivattam@students.iiserpune.ac.in>
2021-02-25 16:39:05 +0530

    rds decoupled

--- examPortal/settings.py
+++ examPortal/settings.py
@@ -76,24 +76,15 @@ WSGI_APPLICATION = 'examPortal.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'postgres',
+        'PASSWORD': 'mimamsaAWS14',
+        'HOST': 'database-1.cxa9mi9i4k9z.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+}
 
 
 AUTH_USER_MODEL = "examPortalApp.User"
