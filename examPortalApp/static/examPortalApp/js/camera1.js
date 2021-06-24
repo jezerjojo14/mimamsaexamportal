43e9ab044c42cd215ec88cb86f3c4767ab19ea06
jezerojojo14 <jezer.vallivattam@students.iiserpune.ac.in>
2021-06-24 23:08:18 +0530

    upload_img

---
+++ examPortalApp/static/examPortalApp/js/camera1.js
@@ -0,0 +1,120 @@
+
+  var width = 320; // We will scale the photo width to this
+  var height = 0; // This will be computed based on the input stream
+
+  var streaming = false;
+
+  var videoEl = null;
+  var canvas = null;
+  var photo = null;
+  var startbutton = null;
+
+  function camStartup() {
+    console.log("camStartup");
+    videoEl = document.getElementById('video');
+    canvas = document.getElementById('canvas');
+    photo = document.getElementById('photo');
+    startbutton = document.getElementById('startbutton');
+    // videoEl.play();
+    // height = videoEl.videoHeight / (videoEl.videoWidth / width);
+    //
+    // if (isNaN(height)) {
+    //   console.log(videoEl.videoHeight);
+    //   console.log(videoEl.videoWidth);
+    //   height = width / (4 / 3);
+    // }
+    //
+    // videoEl.setAttribute('width', width);
+    // videoEl.setAttribute('height', height);
+    // canvas.setAttribute('width', width);
+    // canvas.setAttribute('height', height);
+    // photo.setAttribute('width', width);
+    // photo.setAttribute('height', height);
+    // document.getElementById('close-cam-btn').onclick=()=>{
+    //   videoEl.pause();
+    //   closePopup(document.getElementById('close-cam-btn'));
+    // }
+
+    navigator.mediaDevices.getUserMedia({
+        video: {
+          facingMode: 'environment',
+        },
+        audio: false
+      })
+      .then(function(stream) {
+        videoEl.srcObject = stream;
+        videoEl.play();
+        document.getElementById('close-cam-btn').onclick=()=>{
+          videoEl.pause();
+          stream.getTracks().forEach(function(track) {
+            track.stop();
+          });
+          closePopup(document.getElementById('close-cam-btn'));
+        }
+      })
+      .catch(function(err) {
+        console.error("An error occurred: " + err);
+      });
+
+    videoEl.addEventListener('canplay', function(ev) {
+      console.log("canplay");
+      if (!streaming) {
+        height = videoEl.videoHeight / (videoEl.videoWidth / width);
+
+        if (isNaN(height)) {
+          console.log(videoEl.videoHeight);
+          console.log(videoEl.videoWidth);
+          height = width / (4 / 3);
+        }
+
+        videoEl.setAttribute('width', width);
+        videoEl.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        photo.setAttribute('width', width);
+        photo.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    startbutton.addEventListener('click', function(ev) {
+      console.log("Clicked");
+      takepicture();
+      ev.preventDefault();
+    }, false);
+
+    clearphoto();
+    // openCameraPopup();
+  }
+
+
+  function clearphoto() {
+    console.log("clearphoto");
+    var context = canvas.getContext('2d');
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+  function takepicture() {
+    console.log("takepicture");
+    var context = canvas.getContext('2d');
+    if (width && height) {
+      console.log("width and height defined");
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(videoEl, 0, 0, width, height);
+
+      var data = canvas.toDataURL('image/png');
+      photo.setAttribute('src', data);
+    } else {
+        clearphoto();
+    }
+  }
+
+  function uploadWebcamPicture() {
+    var data = canvas.toDataURL('image/png');
+    sendWebcamData(data);
+  }
