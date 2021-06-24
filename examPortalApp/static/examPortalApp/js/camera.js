3decaf679695ac27c20a92114581346f5511c3a5
jezerojojo14 <jezer.vallivattam@students.iiserpune.ac.in>
2021-06-24 14:24:35 +0530

    camera.js doesn't attempt getUserMedia

--- examPortalApp/static/examPortalApp/js/camera.js
+++ examPortalApp/static/examPortalApp/js/camera.js
@@ -16,32 +16,34 @@
     photo = document.getElementById('photo');
     startbutton = document.getElementById('startbutton');
 
-    navigator.mediaDevices.getUserMedia({
-        video: {
-          facingMode: 'environment',
-        },
-        audio: false
-      })
-      .then(function(stream) {
-        videoEl.srcObject = stream;
-        videoEl.play();
-        document.getElementById('close-cam-btn').onclick=()=>{
-          videoEl.pause();
-          stream.getTracks().forEach(function(track) {
-            track.stop();
-          });
-          closePopup(document.getElementById('close-cam-btn'));
-        }
-      })
-      .catch(function(err) {
-        console.error("An error occurred: " + err);
-      });
+    // navigator.mediaDevices.getUserMedia({
+    //     video: {
+    //       facingMode: 'environment',
+    //     },
+    //     audio: false
+    //   })
+    //   .then(function(stream) {
+    //     videoEl.srcObject = stream;
+    //     videoEl.play();
+    //     document.getElementById('close-cam-btn').onclick=()=>{
+    //       videoEl.pause();
+    //       stream.getTracks().forEach(function(track) {
+    //         track.stop();
+    //       });
+    //       closePopup(document.getElementById('close-cam-btn'));
+    //     }
+    //   })
+    //   .catch(function(err) {
+    //     console.error("An error occurred: " + err);
+    //   });
 
     videoEl.addEventListener('canplay', function(ev) {
       if (!streaming) {
         height = videoEl.videoHeight / (videoEl.videoWidth / width);
 
         if (isNaN(height)) {
+          console.log(videoEl.videoHeight);
+          console.log(videoEl.videoWidth);
           height = width / (4 / 3);
         }
 
