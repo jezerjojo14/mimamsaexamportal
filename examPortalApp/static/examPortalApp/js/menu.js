e6df362a68d8ec6c653a81f277ca6e6d12b27162
jezerojojo14 <jezer.vallivattam@students.iiserpune.ac.in>
2021-06-26 14:21:34 +0530

    chat notifications

--- examPortalApp/static/examPortalApp/js/menu.js
+++ examPortalApp/static/examPortalApp/js/menu.js
@@ -1,10 +1,12 @@
 // Collapsable sidepan
 function openNav() {
+    document.getElementById('chat-logo').style.fill="#59d56a"
     document.getElementById("menu").classList.add("opened");
     document.getElementById("menu").classList.remove("closed");
 }
 
 function closeNav() {
+    document.getElementById('chat-logo').style.fill="#59d56a";
     document.getElementById("menu").classList.add("closed");
     document.getElementById("menu").classList.remove("opened");
 }
